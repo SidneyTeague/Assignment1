97091d8c4a61f45a10b0d3341218ae694662d55f
SidneyTeague <97697893+SidneyTeague@users.noreply.github.com>
2023-05-20 13:29:56 -0400

    Add files via upload

---
+++ Assignment1/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Assignment1
+//
+//  Created by Sidney Teague on 5/20/23.
+//  CPSC 5330 Assignment 1
+//
+//  Running on Xcode 12.4 and testing on an iPhone 12 Pro
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
